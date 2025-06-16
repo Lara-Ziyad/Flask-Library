b3c341360dd5a9b28868c9ec9972e0e231c28f9b
Lara Kamper <lailacaty@Mac.fritz.box>
2025-06-16 15:06:20 +0200

    create: add author route

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request
 
-# Import the database object and models (we'll create these in data_models.py)
-# from data_models import db, Author, Book
+# Import the database object and models
+from data_models import db, Author, Book
 
 app = Flask(__name__)
 
 # Create db object
-db = SQLAlchemy()
+
 
 # Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
@@ -15,6 +14,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Bind the app to the SQLAlchemy object
 db.init_app(app)
+
+# Route to add an author
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        name = request.form['name']
+        birth_date = request.form['birth_date']
+        date_of_death = request.form['date_of_death']
+
+        author = Author(
+            name=name,
+            birth_date=birth_date or None,
+            date_of_death=date_of_death or None
+        )
+        db.session.add(author)
+        db.session.commit()
+
+    return render_template('')
 
 if __name__ == '__main__':
     # Create tables
