1f00761cd63220e3c85ed04325b9cc5886a92c7b
Lara Kamper <lailacaty@Mac.fritz.box>
2025-06-16 13:30:54 +0200

    add: data_models file include author model

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Create db object
+db = SQLAlchemy()
+
 # Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Bind the app to the SQLAlchemy object
-# db.init_app(app)
+db.init_app(app)
---
+++ data_models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize SQLAlchemy object
+db = SQLAlchemy()
+
+
+# Author model
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.Date, nullable=True)
+    date_of_death = db.Column(db.Date, nullable=True)
+
+    def __repr__(self):
+        return f"<Author {self.name}>"
+
+    def __str__(self):
+        return self.name
+
+
