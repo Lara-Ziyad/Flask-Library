3e863ccc64a693819fff394df9001d8283aa6fc8
Lara Kamper <lailacaty@Mac.fritz.box>
2025-06-16 13:57:47 +0200

    add: book model

--- data_models.py
+++ data_models.py
@@ -11,10 +11,30 @@ class Author(db.Model):
     birth_date = db.Column(db.Date, nullable=True)
     date_of_death = db.Column(db.Date, nullable=True)
 
+    # Relationship to Book model
+    books = db.relationship('Book', backref='author', lazy=True)
+
     def __repr__(self):
         return f"<Author {self.name}>"
 
     def __str__(self):
         return self.name
+
+
+# Book model
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(20), unique=True, nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer)
+
+    # Foreign key to Author table
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
+
+    def __str__(self):
+        return self.title
 
 
